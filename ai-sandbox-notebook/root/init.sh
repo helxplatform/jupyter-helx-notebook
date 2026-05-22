d35c00d8ee1c6c05db3b7d432677c708776f8c5c
frostyfan109 <groupe@renci.org>
2026-05-22 12:44:26 -0400

    Add openpyxl and revert UNC hackathon changes to base notebook

--- ai-sandbox-notebook/root/init.sh
+++ ai-sandbox-notebook/root/init.sh
@@ -17,36 +17,13 @@ export USER=${USER-"jovyan"}
 export DEFAULT_USER="jovyan"
 export HOME="/home/$USER"
 export NB_ROOT_DIR=${NB_ROOT_DIR-$HOME}
-export JUPYTER_AI_DEFAULT_MODEL=${JUPYTER_AI_DEFAULT_MODEL-"openai/gpt-5"}
-export JUPYTER_AI_DEFAULT_PERSONA=${JUPYTER_AI_DEFAULT_PERSONA-"jupyter-ai-personas::goodbot::GoodBotPersona"}
-export GOODBOT_STORE_IDS_PATH="$HOME/.goodbot/store_ids.json"
 # Set a custom prompt for the user that's more pleasant to read in the terminal
 export PROMPT_COMMAND='PS1="\[\033[32m\]\u\[\033[00m\]:\[\033[34m\]\w\[\033[00m\]\$ "'
 
-# Clone the hackathon repo into the user's home directory if it doesn't already
-# exist, then switch to a user-specific branch (creating it if needed).
-HACKATHON_REPO="https://github.com/RENCI/AI-For-Public-Good-Hackathon"
-HACKATHON_DIR="$HOME/AI-For-Public-Good-Hackathon"
-if [ ! -d "$HACKATHON_DIR" ]; then
-  echo "Cloning hackathon repo into $HACKATHON_DIR"
-  git clone "$HACKATHON_REPO" "$HACKATHON_DIR"
-fi
-cd "$HACKATHON_DIR"
-USERNAME=${USERNAME-$USER}
-if git show-ref --verify --quiet "refs/heads/$USERNAME"; then
-  echo "Switching to existing branch $USERNAME"
-  git checkout "$USERNAME"
-elif git show-ref --verify --quiet "refs/remotes/origin/$USERNAME"; then
-  echo "Checking out remote branch $USERNAME"
-  git checkout -b "$USERNAME" "origin/$USERNAME"
-else
-  echo "Creating new branch $USERNAME"
-  git checkout -b "$USERNAME"
-fi
-
-# Change to the root directory to mitigate problems if the current working
-# directory is deleted.
-cd /
+#####################
+# Jupyter AI config #
+#####################
+export JUPYTER_AI_DEFAULT_MODEL=${JUPYTER_AI_DEFAULT_MODEL-"openai/gpt-5"}
 
 # Add other init scripts in $HELX_SCRIPTS_DIR with ".sh" as their extension.
 # To run in a certain order, name them appropriately.
@@ -59,8 +36,7 @@ do
 done
 
 # Change CWD to /home/$USER so it is the starting point for shells in jupyter.
-# cd $HOME
-cd $HACKATHON_DIR
+cd $HOME
 
 # The default for XDG_CACHE_HOME to use /home/jovyan/.cache and jupyter will
 # create the directory if it doesn't exist.
@@ -73,8 +49,7 @@ jupyter lab \
     --ServerApp.ip='*' \
     --ServerApp.base_url=${NB_PREFIX} \
     --ServerApp.allow_origin="*" \
-    --ServerApp.root_dir=${HACKATHON_DIR} \
+    --ServerApp.root_dir=${NB_ROOT_DIR} \
     --ServerApp.default_url=${NB_PREFIX}/lab \
     --AiExtension.default_language_model=${JUPYTER_AI_DEFAULT_MODEL} \
-    --AiExtension.initial_language_model=${JUPYTER_AI_DEFAULT_MODEL} \
-    --PersonaManager.default_persona_id=${JUPYTER_AI_DEFAULT_PERSONA}
+    --AiExtension.initial_language_model=${JUPYTER_AI_DEFAULT_MODEL}
